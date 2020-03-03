d93dc05db8ffb4c782240dfbdba0775452d68be1
Adrian Juhl <adrian.juhl@gmail.com>
2020-03-03 21:29:07 +1030

    Adjusted parameter names and variable names.

--- cicd/test-scripts/dev.sh
+++ cicd/test-scripts/dev.sh
@@ -4,25 +4,25 @@ function usage
 {
   echo "usage: dev.sh <options>"
   echo "where options are:"
-  echo "    --applicationName    applicationName    the application name"
+  echo "    --application-name   applicationName    the application name"
   echo "    --namespace          namespace          the Openshift namespace hosting the application"
-  echo "    --ocpHostnameBase    ocpHostnameBase    the OCP hostname base/suffix"
+  echo "    --ocp-hostname-base  ocpHostnameBase    the OCP hostname base/suffix"
 }
 
-applicationName=
-namespace=
-ocpHostnameBase=
+APPLICATION_NAME=
+NAMESPACE=
+OCP_HOSTNAME_BASE=
 
 while [ "$1" != "" ]; do
   case $1 in
-    --applicationName )                shift
-                                       applicationName=$1
+    --application-name )                shift
+                                       APPLICATION_NAME=$1
                                        ;;
     --namespace )                      shift
-                                       namespace=$1
+                                       NAMESPACE=$1
                                        ;;
-    --ocpHostnameBase )                shift
-                                       ocpHostnameBase=$1
+    --ocp-hostname-base )              shift
+                                       OCP_HOSTNAME_BASE=$1
                                        ;;
     --help )                           usage
                                        exit
@@ -35,28 +35,28 @@ while [ "$1" != "" ]; do
 done
 
 # Check that all options were provided
-if [ -z "$applicationName" ]; then
-  echo "ERROR: option --applicationName was not provided"
+if [ -z "${APPLICATION_NAME}" ]; then
+  echo "ERROR: option --application-name was not provided"
   usage
   exit 1
 fi
-if [ -z "$namespace" ]; then
+if [ -z "${NAMESPACE}" ]; then
   echo "ERROR: option --namespace was not provided"
   usage
   exit 1
 fi
-if [ -z "$ocpHostnameBase" ]; then
-  echo "ERROR: option --ocpHostnameBase was not provided"
+if [ -z "${OCP_HOSTNAME_BASE}" ]; then
+  echo "ERROR: option --ocp-hostname-base was not provided"
   usage
   exit 1
 fi
 
-echo "applicationName is ${applicationName}"
-echo "namespace is ${namespace}"
-echo "ocpHostnameBase is ${ocpHostnameBase}"
+echo "APPLICATION_NAME is ${APPLICATION_NAME}"
+echo "NAMESPACE is ${NAMESPACE}"
+echo "OCP_HOSTNAME_BASE is ${OCP_HOSTNAME_BASE}"
 
 readinessprobe_reply_expected="ready"
-readinessprobe_reply="$( curl -s https://${applicationName}-${namespace}.${ocpHostnameBase}/api/readinessprobe )"
+readinessprobe_reply="$( curl -s https://${APPLICATION_NAME}-${NAMESPACE}.${OCP_HOSTNAME_BASE}/api/readinessprobe )"
 echo "readinessprobe_reply is ${readinessprobe_reply}"
 if [ "${readinessprobe_reply}" != "${readinessprobe_reply_expected}" ]; then
   echo "ERROR: readinessprobe did not reply with \"${readinessprobe_reply_expected}\" but instead replied with: ${readinessprobe_reply}"
